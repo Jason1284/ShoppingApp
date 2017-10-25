4bc4b0a5bddd8a9936efe23f64ceb14b6cbc2266
Jason1284 <Jason1284@gmail.com>
2017-10-24 21:24:01 -0600

    Lets do this.

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is Jason's comment! He has the best change comments.
+        //This is Jason's comment! I hope this works!
         //This is Martin's comment!
         //This is James' comment!
     }
