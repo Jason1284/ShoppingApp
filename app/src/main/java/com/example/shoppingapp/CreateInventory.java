997869f5074fb807e3182db3238005f4ab709f2e
jclarke4 <cla16054@byui.edu>
2017-11-03 13:54:16 -0600

    New Interface Design James' part and java activities

---
+++ app/src/main/java/com/example/shoppingapp/CreateInventory.java
@@ -0,0 +1,15 @@
+package com.example.shoppingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by James on 11/3/2017.
+ */
+
+public class CreateInventory extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_inventory);
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/EditProduct.java
@@ -0,0 +1,16 @@
+package com.example.shoppingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by James on 11/3/2017.
+ */
+
+public class EditProduct extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_modify_product);
+    }
+
+}
