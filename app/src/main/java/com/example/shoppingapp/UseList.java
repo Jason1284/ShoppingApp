4360e3eab14d164bef5ac25503fe51e5dcde4228
Jason1284 <Jason1284@gmail.com>
2017-11-04 15:13:17 -0600

    Created the choose_inventory and create_list activities.

---
+++ app/src/main/java/com/example/shoppingapp/UseList.java
@@ -0,0 +1,17 @@
+package com.example.shoppingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Jason on 20171104.
+ */
+
+public class UseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_list);
+    }
+}
