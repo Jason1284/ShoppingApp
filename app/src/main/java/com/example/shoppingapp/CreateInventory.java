4b300de5e49cd580204cc4991b1b955fad76bdcd
martincornelli <32455478+martincornelli@users.noreply.github.com>
2017-11-07 18:09:35 -0300

    Send inventory name

--- app/src/main/java/com/example/shoppingapp/CreateInventory.java
+++ app/src/main/java/com/example/shoppingapp/CreateInventory.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateInventory extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.shoppingapp.MESSAGE";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_inventory);
@@ -15,6 +17,9 @@ public class CreateInventory extends AppCompatActivity {
 
     public void onNewInventory(View view){
         Intent intent = new Intent(this, UseInventory.class);
+        EditText editText = (EditText) findViewById(R.id.editText4);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
