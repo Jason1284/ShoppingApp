daecb9845cd66d00ec0d2806390f6fd303fbd6ac
martincornelli <32455478+martincornelli@users.noreply.github.com>
2017-11-07 18:06:49 -0300

    Send list name to next activity

--- app/src/main/java/com/example/shoppingapp/create_list.java
+++ app/src/main/java/com/example/shoppingapp/create_list.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by Jason on 20170311.
  */
 
 public class create_list extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.shoppingapp.MESSAGE";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
@@ -19,6 +21,9 @@ public class create_list extends AppCompatActivity {
 
     public void onNewList(View view){
         Intent intent = new Intent(this, UseList.class);
+        EditText editText = (EditText) findViewById(R.id.editText104);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
