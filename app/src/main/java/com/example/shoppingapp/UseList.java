6aae264570d13bf6d45bd094c56205acb5c3a1cc
Jason1284 <Jason1284@gmail.com>
2017-11-18 08:34:07 -0700

    Started first step in creating an interactive listview. 3rd try.

--- app/src/main/java/com/example/shoppingapp/UseList.java
+++ app/src/main/java/com/example/shoppingapp/UseList.java
@@ -1,7 +1,10 @@
 package com.example.shoppingapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.app.ListActivity;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,9 @@ import java.util.ArrayList;
 public class UseList extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.shoppingapp.MESSAGE";
     public static String FORWARD;
+    Cursor cursor;
+    CustomCursorAdapter myCursorAdapter;
+    ContentValues values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,33 @@ public class UseList extends AppCompatActivity {
         SharedPreferences.Editor editor = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE).edit();
         editor.putString("name", message);
         editor.apply();
+
+        /*
+        private void createListView() {
+            setContentView(R.layout.main);
+
+            cursor=dh.query(DatabaseHelper.USER_PASSWORD, new String[]{“_id”,”username”,”selected”}, null, null, null, null, “firstname asc”);
+
+            startManagingCursor(cursor);
+            myCursorAdapter= new CustomCursorAdapter(this,cursor);
+            this.getListView().setAdapter(myCursorAdapter);
+        }
+
+        private void insertData(String firstName ,String password){
+            if(values!= null){
+                values.clear();
+            }
+            values.put(“username”,firstName);
+            values.put(“password”, password);
+            dh.insert(DatabaseHelper.USER_PASSWORD, null, values);
+        }
+
+        private void clickHandler(View view){
+
+            if(view.getId() == R.id.checkbox){
+                cursor.requery();
+            }
+        }*/
 
 
     }
